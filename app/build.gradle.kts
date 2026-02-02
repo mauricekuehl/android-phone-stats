a834421065db87cce3112f4119029f9c1fdea652
Maurice Kuehl <dev@mauricekuehl.com>
2026-02-02 14:52:12 +0100

    add signing to the build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,19 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = "android"
+            keyAlias = "phonestats"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
